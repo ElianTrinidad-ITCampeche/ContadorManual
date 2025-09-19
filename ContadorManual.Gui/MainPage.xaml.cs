bcfa194075b13b5b5d88631cdf64f565cfdc222a
Elian Emanuel Trinidad Dzib <24470028@campeche.tecnm.mx>
2025-09-19 11:01:10 -0600

    Versión que utiliza VerticalStackLayout como elemento raíz

---
+++ ContadorManual.Gui/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ContadorManual.Gui
+{
+    public partial class MainPage : ContentPage
+    {
+        //_conteo lleva el conteo de la apicación
+        private int _conteo;
+        public MainPage()
+        {
+            InitializeComponent();
+            _conteo = 5;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _conteo++;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            _conteo = 0;
+            ConteoLabel.Text = _conteo.ToString();
+        }
+    }
+}
